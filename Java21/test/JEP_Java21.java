fcb4fef13c5ef265189ad7e55060d0ddf3cada7d
Jean-Luc VASSENT <vassent.jl@free.fr>
2025-02-21 16:18:13 +0100

    Add Test for Virtual Thread

--- Java21/test/JEP_Java21.java
+++ Java21/test/JEP_Java21.java
@@ -1,6 +1,9 @@
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import java.time.Duration;
+import java.util.concurrent.Executors;
 import java.util.random.RandomGeneratorFactory;
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 
@@ -25,5 +28,23 @@ public class JEP_Java21 { // --add-modules jdk.incubator.vector at runtime
         var cVectorized = aVectorized.add(bVectorized);
 
         assertArrayEquals(expected, cVectorized.toArray());
+    }
+
+    @Test
+    public void testVirtualThread() {
+        final var factory = Thread.ofVirtual().name("VT ", 0).factory();
+        var nbCore = Runtime.getRuntime().availableProcessors();
+        try (var executor = Executors.newThreadPerTaskExecutor(factory)) {
+           IntStream.range(0, nbCore*2)
+                   .forEachOrdered(i -> executor.submit(() -> {
+                       var thread = Thread.currentThread().toString();
+                       System.out.println(String.format("Virtual TH %d on %S", i, thread));
+                       try {
+                           Thread.sleep(Duration.ofSeconds(1L));
+                       } catch (InterruptedException e) {
+                           throw new RuntimeException(e);
+                       }
+                   }));
+        }
     }
 }
